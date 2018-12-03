c2e983c539907e4bfb7b62cab7b2b4fce17c085c
Daiki Ikeda <tetra.chan.da.z.1000@gmail.com>
2018-12-03 23:39:38 +0900

    Simplify js

--- js/script.js
+++ js/script.js
@@ -4,17 +4,16 @@ document.addEventListener('DOMContentLoaded', () => {
 
     for (let i = 0; i < articles.length; i++) {
 
-        const article = articles[i];
-        const url = article.getElementsByTagName('a')[0].href.split('/')[3];
+        const url = articles[i].getElementsByTagName('a')[0].href.split('/')[3];
 
-        article.addEventListener('mouseenter', () => {
+        articles[i].addEventListener('mouseenter', () => {
             document.body.style.backgroundImage = 'url(/image/' + url + '.gif)';
             for (let j = 0; j < articles.length; j++) {
                 articles[j].classList.toggle(i == j ? 'hover' : 'hidden');
             }
         });
 
-        article.addEventListener('mouseleave', () => {
+        articles[i].addEventListener('mouseleave', () => {
             document.body.style.backgroundImage = 'none';
             for (let j = 0; j < articles.length; j++) {
                 articles[j].classList.toggle(i == j ? 'hover' : 'hidden');
