52bff0a9133d6236153f4408068a32125157849c
Daiki Ikeda <tetra.chan.da.z.1000@gmail.com>
2018-12-05 23:03:41 +0900

    Improve hovering

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
-document.addEventListener('DOMContentLoaded', () => {
+window.addEventListener('load', () => {
 
     const articles = document.getElementsByClassName('article');
+    const articleArray = Array.from(articles);
+
+    let x = 0;
+    let y = 0;
 
     for (let i = 0; i < articles.length; i++) {
 
@@ -9,42 +13,45 @@ document.addEventListener('DOMContentLoaded', () => {
         articles[i].addEventListener('mouseenter', () => {
             document.body.style.backgroundImage = 'url(/image/' + url + '.gif)';
             for (let j = 0; j < articles.length; j++) {
-                articles[j].classList.toggle(i == j ? 'hover' : 'hidden');
+                articles[j].classList.add(i == j ? 'hover' : 'hidden');
             }
         });
 
         articles[i].addEventListener('mouseleave', () => {
             document.body.style.backgroundImage = 'none';
             for (let j = 0; j < articles.length; j++) {
-                articles[j].classList.toggle(i == j ? 'hover' : 'hidden');
+                articles[j].classList.remove('hover', 'hidden');
             }
         });
 
     }
-});
-
-window.addEventListener('load', () => {
-
-    let x = 0;
-    let y = 0;
-    let lastArticle = '';
 
     document.addEventListener('mousemove', (e) => {
+
         x = e.clientX;
         y = e.clientY;
+
     });
 
     window.addEventListener('scroll', () => {
-        /*
+
         const element = document.elementFromPoint(x, y);
-        const article = element.parentElement.parentElement;
-        if (article.classList.contains('article')) {
-            article.classList.add('hover');
-            lastArticle = article;
+        const article = element.parentElement.parentElement || document.body;
+        const index = articleArray.indexOf(article);
+
+        if (index + 1) {
+            const url = article.getElementsByTagName('a')[0].href.split('/')[3];
+            document.body.style.backgroundImage = 'url(/image/' + url + '.gif)';
+            for (let i = 0; i < articles.length; i++) {
+                articles[i].classList.add(i == index ? 'hover' : 'hidden');
+            }
         } else {
-            lastArticle.classList.remove('hover');
+            document.body.style.backgroundImage = 'none';
+            for (let i = 0; i < articles.length; i++) {
+                articles[i].classList.remove('hover', 'hidden');
+            }
         }
-        */
-        console.log(window.scrollY);
+
     });
+
 });
